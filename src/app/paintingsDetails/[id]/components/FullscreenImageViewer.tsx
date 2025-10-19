205e579fb738d759977e678013cbf81f8f643d99
Joseph Kawe <kohako.tech@gmail.com>
2025-10-19 02:17:54 -0300

    refactor(painting details): add new fullscreen viewer components with improvements

--- src/app/paintingsDetails/[id]/components/FullscreenImageViewer.tsx
+++ src/app/paintingsDetails/[id]/components/FullscreenImageViewer.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRef } from "react";
-import Image from "next/image";
 import LightGallery from "lightgallery/react";
 import { LightGallery as ILightGallery } from "lightgallery/lightgallery";
 import lgZoom from "lightgallery/plugins/zoom";
@@ -13,40 +12,35 @@ import "lightgallery/css/lg-fullscreen.css";
 
 import { FullscreenImageViewerProps } from "@/types/paintingDetails.type";
 
-export function FullscreenImageViewer({ painting, onClose }: FullscreenImageViewerProps) {
+export function FullscreenImageViewer({
+  painting,
+  onClose,
+}: FullscreenImageViewerProps) {
   const lightGalleryRef = useRef<ILightGallery | null>(null);
 
+  const galleryItems = [
+    {
+      src: painting.imagePainting,
+      subHtml: `<h4>${painting.namePainting}</h4><p>${painting.datePainting}</p>`,
+    },
+  ];
+
   return (
-    <div>
-      <LightGallery
-        onInit={(detail) => {
-          if (detail) {
-            lightGalleryRef.current = detail.instance;
-            setTimeout(() => detail.instance.openGallery(0), 50);
-          }
-        }}
-        onAfterClose={() => onClose?.()}
-        closable
-        counter={false}
-        plugins={[lgZoom, lgFullscreen]}
-        speed={500}
-        download={false}
-      >
-        <a
-          style={{ display: "none" }}
-          data-src={painting.imagePainting}
-          key={painting.namePainting}
-          data-sub-html={`<h4>${painting.namePainting}</h4><p>${painting.datePainting}</p>`}
-        >
-          <Image
-            src={painting.imagePainting}
-            alt={painting.alt}
-            width={100}
-            height={100}
-            quality={100}
-          />
-        </a>
-      </LightGallery>
-    </div>
+    <LightGallery
+      onInit={(detail) => {
+        if (detail) {
+          lightGalleryRef.current = detail.instance;
+          setTimeout(() => detail.instance.openGallery(0), 50);
+        }
+      }}
+      onAfterClose={() => onClose?.()}
+      closable
+      counter={false}
+      plugins={[lgZoom, lgFullscreen]}
+      speed={500}
+      download={false}
+      dynamic={true}
+      dynamicEl={galleryItems}
+    />
   );
 }
